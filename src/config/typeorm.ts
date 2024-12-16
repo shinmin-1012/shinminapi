a7df31db9fd14c7e186029358b9a4f740e85faf8
shinmin <shinminproject@gmail.com>
2024-12-16 16:22:49 +0700

    connect db

---
+++ src/config/typeorm.ts
@@ -0,0 +1,26 @@
+import { registerAs } from '@nestjs/config';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig({ path: '.env' });
+
+const DATABASE_HOST = process.env.DATABASE_HOST ?? '34.143.189.199';
+const DATABASE_PORT = process.env.DATABASE_PORT ?? 5432;
+const USER_NAME = process.env.DATABASE_USERNAME ?? 'tuanhta';
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD ?? ':BMAt4RtL"YCa,)@';
+const DATABASE_NAME = process.env.DATABASE_NAME ?? 'study';
+
+const config = {
+  type: 'postgres',
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  username: USER_NAME,
+  password: DATABASE_PASSWORD,
+  database: DATABASE_NAME,
+  entities: [__dirname, '/modules/**/*.entity.{ts,js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'], // Adjust path to migrations
+  autoLoadEntities: true,
+  synchronize: false,
+  logging: true,
+};
+
+export default registerAs('typeorm', () => config);
