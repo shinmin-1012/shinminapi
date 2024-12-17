0f38bfb1414047eeee26693102e1836a1cfe19a9
shinmin <shinminproject@gmail.com>
2024-12-17 14:21:14 +0700

    add config map

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  /**
+   *
+   */
+  constructor(private readonly configService: ConfigService) {}
   getHello(): string {
-    return 'Hello From API!';
+    const value = this.configService.get<string>('YOUR_ENV_VARIABLE');
+    return 'Get value from env => ' + value;
   }
 }
